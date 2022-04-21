05c1808efe0fb2d7637ca37640a3ded3622dfa8e
autodotua <autodotua@outlook.com>
2022-04-21 09:22:59 +0800

    修复了捕捉失效的BUG

--- MapBoard.UI/Mapping/EditorHelper.cs
+++ MapBoard.UI/Mapping/EditorHelper.cs
@@ -427,8 +427,7 @@ namespace MapBoard.Mapping
             var position = e.GetPosition(MapView);
             var location = MapView.ScreenToLocation(position);
             if (!CanCatchNearestPoint()//没开
-                || MapView.CurrentTask != BoardTask.Draw//没有正在画质
-                || e.LeftButton == MouseButtonState.Pressed//鼠标左键按下了
+                || MapView.CurrentTask != BoardTask.Draw//没有正在绘制
                 || IsMouseNearSketch(location))//距离正在绘制的图形太近
             {
                 MapView.Overlay.SetNearestPointPoint(null);
