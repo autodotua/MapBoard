5b0a46df3a4ea3bfecd906856c434e89c26d0be0
autodotua <autodotua@outlook.com>
2023-08-24 10:51:32 +0800

    更新脚本

--- copyRuntimes.ps1
+++ copyRuntimes.ps1
@@ -1,2 +1,4 @@
-Copy-Item C:\Users\$($env:USERNAME)\.nuget\packages\esri.arcgisruntime.runtimes.win10\200.0.0\runtimes\win10-x64\native\* MapBoard.UI
-Copy-Item C:\Users\$($env:USERNAME)\.nuget\packages\esri.arcgisruntime.wpf\200.0.0\runtimes\win10-x64\native\* MapBoard.UI
+Copy-Item C:\Users\$($env:USERNAME)\.nuget\packages\esri.arcgisruntime.runtimes.win10\200.2.0\runtimes\win10-x64\native\* MapBoard.UI
+Copy-Item C:\Users\$($env:USERNAME)\.nuget\packages\esri.arcgisruntime.wpf\200.2.0\runtimes\win10-x64\native\* MapBoard.UI
+Write-Output "复制完成"
+pause
