564fa90cf547fcd991d9e94dc3ee8554a543f503
autodotua <autodotua@outlook.com>
2023-10-05 21:32:37 +0800

    创建MAUI项目，边学习边写，计划是用于代替手机端的GIS工具箱

---
+++ MapBoard.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MapBoard.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MapBoard.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MapBoard.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
