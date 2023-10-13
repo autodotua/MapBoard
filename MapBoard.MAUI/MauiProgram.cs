dbcec3d87d331dd2c667e74043c9d7c868c3ae7c
autodotua <autodotua@outlook.com>
2023-10-13 14:28:09 +0800

    增加地图元素

--- MapBoard.MAUI/MauiProgram.cs
+++ MapBoard.MAUI/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Esri.ArcGISRuntime.Maui;
+using Esri.ArcGISRuntime.Toolkit.Maui;
 using FubarDev.FtpServer;
 using FubarDev.FtpServer.FileSystem.DotNet;
 using MapBoard.Services;
@@ -19,31 +20,28 @@ namespace MapBoard
                 .ConfigureLifecycleEvents(events =>
                 {
 #if ANDROID
-                events.AddAndroid(android => android.OnCreate((activity, bundle) => MakeStatusBarTranslucent(activity)));
+                    events.AddAndroid(android => android.OnCreate((activity, bundle) => MakeStatusBarTranslucent(activity)));
 
-                static void MakeStatusBarTranslucent(Android.App.Activity activity)
-                {
-                    activity.Window.SetFlags(Android.Views.WindowManagerFlags.LayoutNoLimits, Android.Views.WindowManagerFlags.LayoutNoLimits);
+                    static void MakeStatusBarTranslucent(Android.App.Activity activity)
+                    {
+                        activity.Window.SetFlags(Android.Views.WindowManagerFlags.LayoutNoLimits, Android.Views.WindowManagerFlags.LayoutNoLimits);
 
-                    activity.Window.ClearFlags(Android.Views.WindowManagerFlags.TranslucentStatus);
+                        activity.Window.ClearFlags(Android.Views.WindowManagerFlags.TranslucentStatus);
 
-                    activity.Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
-                }
+                        activity.Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
+                    }
 #endif
                 })
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
-            builder.UseArcGISRuntime();
+                })
+                .UseArcGISRuntime()
+                .UseArcGISToolkit();
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
-            FtpService.GetIpAddress();
-
-
-
             return builder.Build();
         }
     }
