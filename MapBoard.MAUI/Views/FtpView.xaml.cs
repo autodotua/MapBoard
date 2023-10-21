5f0d71190b475d7160956ddb5f881edc350c6817
autodotua <autodotua@outlook.com>
2023-10-21 15:00:49 +0800

    增加导出轨迹到Downloads目录的功能（未完成）

--- MapBoard.MAUI/Views/FtpView.xaml.cs
+++ MapBoard.MAUI/Views/FtpView.xaml.cs
@@ -15,12 +15,15 @@ public partial class FtpView : ContentView, ISidePanel
     {
         BindingContext = new FtpViewViewModel();
         InitializeComponent();
-        if (DeviceInfo.Platform == DevicePlatform.WinUI)
-        {
-            btnFtp.IsVisible = false;
+#if WINDOWS
+        btnFtp.IsVisible = false;
             btnOpenDir.IsVisible = true;
             grdFtpInfo.IsVisible = false;
-        }
+#endif
+
+#if !ANDROID
+    lblSaveGpx.IsVisible=false;
+#endif
     }
 
     public SwipeDirection Direction => SwipeDirection.Left;
@@ -72,12 +75,12 @@ public partial class FtpView : ContentView, ISidePanel
 
     private void OpenDirButton_Clicked(object sender, EventArgs e)
     {
-        if (DeviceInfo.Platform != DevicePlatform.WinUI)
-        {
-            throw new NotSupportedException("仅支持Windows");
-        }
+#if WINDOWS
         string dir = GetSelectedDir();
         Process.Start("explorer.exe", dir);
+#else
+        throw new NotSupportedException();
+#endif
     }
 
     private async void StartStopFtpButton_Clicked(object sender, EventArgs e)
@@ -98,5 +101,69 @@ public partial class FtpView : ContentView, ISidePanel
             (sender as Button).Text = "关闭FTP";
         }
         (BindingContext as FtpViewViewModel).IsOn = !(BindingContext as FtpViewViewModel).IsOn;
+    }
+
+    private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+        var handle = ProgressPopup.Show("正在保存");
+#if ANDROID
+        try
+        {
+            var root = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryDownloads);
+            root = new Java.IO.File(root, AppInfo.Name);
+            root.Mkdir();
+
+
+
+
+            await Task.Run(() =>
+            {
+                var gpxFiles = Directory.EnumerateFiles(FolderPaths.TrackPath, "*.gpx").ToList();
+                foreach (var name in gpxFiles)
+                {
+                    var dist = Path.Combine(root.Path, Path.GetFileName(name));
+                    File.Copy(name, dist);
+                    File.SetLastWriteTime(dist, File.GetLastWriteTime(name));
+                    //var source = new Java.IO.File(name);
+                    //var dist = new Java.IO.File(root, Path.GetFileName(name));
+                    //if (dist.Exists())
+                    //{
+                    //    continue;
+                    //}
+                    //using Java.IO.InputStream inStream = new Java.IO.FileInputStream(source);
+                    //try
+                    //{
+                    //    var buffer = new byte[1024];
+                    //    int length = 0;
+                    //    using Java.IO.OutputStream outputStream = new Java.IO.FileOutputStream(dist);
+                    //    try
+                    //    {
+                    //        while ((length = await inStream.ReadAsync(buffer)) > 0)
+                    //        {
+                    //            await outputStream.WriteAsync(buffer, 0, length);
+                    //        }
+                    //    }
+                    //    finally
+                    //    {
+                    //        outputStream.Close();
+                    //    }
+                    //}
+                    //finally
+                    //{
+                    //    inStream.Close();
+                    //}
+                }
+            });
+            Android.Widget.Toast.MakeText(Platform.CurrentActivity, "已保存到" + root.Path, Android.Widget.ToastLength.Short).Show();
+        }
+        catch(Exception ex)
+        {
+            await MainPage.Current.DisplayAlert("保存失败", ex.Message, "确定");
+        }
+
+#else
+        throw new NotSupportedException();
+#endif
+        handle.Close();
     }
 }
